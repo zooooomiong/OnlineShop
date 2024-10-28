d383cf904f6ce29a619364aaba41212ce7468d95
nima <ssu87556@gmail.com>
2024-10-29 01:12:50 +0330

    refactor: Change User(add unique to email and phone_number)

--- apps/account/models.py
+++ apps/account/models.py
@@ -6,11 +6,11 @@ from django.core.validators import validate_email
 
 class User(AbstractUser):
     username = models.CharField(_("username"),max_length=100,unique=True,validators=[validate_username])
-    email = models.EmailField(_("email address"), blank=True, validators=[validate_email])
+    email = models.EmailField(_("email address"), blank=True, unique=True, validators=[validate_email])
     password = models.CharField(_("password"), max_length=128, validators=[validate_password])
     first_name = models.CharField(_("First name"), max_length=100, validators=[validate_first_name])
     last_name = models.CharField(_("Last name"), max_length=100, validators=[validate_last_name])
-    phone_number =models.CharField(_("Phone number"), max_length=11, validators=[validate_phone_number])
+    phone_number =models.CharField(_("Phone number"), max_length=11, unique=True, validators=[validate_phone_number])
     is_provider = models.BooleanField(_("Is provider"), default=False)
     is_customer = models.BooleanField(_("Is customer"), default=False)
 
