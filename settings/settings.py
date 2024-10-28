74e21e634a59f6ef8d0f85bcbaf0c7ddba420f40
nima <ssu87556@gmail.com>
2024-10-28 11:43:08 +0330

    chore:Add templates and add bootstrap files

--- settings/settings.py
+++ settings/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = "settings.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
